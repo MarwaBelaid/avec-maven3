6bcc975e9d82db31518888dd292374b5a397ab82
Marwa Belaid <marwa.belaid@esprit.tn>
2023-01-21 15:27:48 +0100

    new new

--- src/main/java/tn/esprit/CallRestWebService.java
+++ src/main/java/tn/esprit/CallRestWebService.java
@@ -60,6 +60,7 @@ public class CallRestWebService {
 			//ip = responseObj.getString("origin");
 			ip = responseObj.getString("query");
 			System.out.println("ip : " + ip); 
+			System.out.println("ip : " + ip); 
 
 		} catch (IOException e) {
 			e.printStackTrace();
